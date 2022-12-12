b4ea8be81b41994edfcdb2cead603d12aa1bc2de
biuybiu <zzzz.officiel@gmail.com>
2022-12-12 20:45:14 +0100

    新文件

--- openai_javascript.py
+++ openai_javascript.py
@@ -13,3 +13,19 @@ response = openai.Completion.create(
     presence_penalty=0.0,
     stop=["You:"],
 )
+
+
+# import os
+# import openai
+
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# response = openai.Completion.create(
+#   model="code-davinci-002",
+#   prompt="",
+#   temperature=0,
+#   max_tokens=256,
+#   top_p=1,
+#   frequency_penalty=0,
+#   presence_penalty=0
+# )
